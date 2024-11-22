8bce539d2556223feff4a0051f8002cd9b8f2a54
Kartik Vijay Shanbhag <shanbhag.k@northeastern.edu>
2024-11-22 03:16:25 -0500

    Contains Duplicate

---
+++ src/ContainsDuplicate/App1.java
@@ -0,0 +1,23 @@
+package ContainsDuplicate;
+
+import java.util.HashSet;
+
+// Optimised Method
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+public class App1 {
+    public static void main(String[] args) {
+        int[] nums={1,2,3,4};
+        System.out.println(hasDuplicate(nums));
+    }
+
+    public static boolean hasDuplicate(int[] nums){
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0;i< nums.length;i++){
+            if(!hs.add(nums[i])){
+                return true;
+            }
+            hs.add(nums[i]);
+        }
+        return false;
+    }
+}
---
+++ src/ContainsDuplicate/App2.java
@@ -0,0 +1,20 @@
+package ContainsDuplicate;
+
+
+//Brute Force Approach
+public class App2 {
+    public static void main(String[] args) {
+        int[] nums={1,2,3,3};
+        System.out.println(hasDuplicate(nums));
+    }
+    public static boolean hasDuplicate(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
